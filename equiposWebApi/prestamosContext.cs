ef1a622228f7dc6b518707c10ea61e35903a08d0
andres lemus <andres.lemus@catolica.edu.sv>
2022-02-19 17:26:53 -0600

    Comentarios

---
+++ equiposWebApi/prestamosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using equiposWebApi.Models;
+
+namespace equiposWebApi
+{
+    public class prestamosContext : DbContext
+    {
+        public prestamosContext(DbContextOptions<prestamosContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<equipos> equipos { get; set; }
+    }
+}
