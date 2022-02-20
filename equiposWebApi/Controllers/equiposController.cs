1dd5d3265b3bcfa9f24d64c4bc54b9998e27e7f6
andres lemus <andres.lemus@catolica.edu.sv>
2022-02-19 19:17:29 -0600

    Finalización de CRUD

--- equiposWebApi/Controllers/equiposController.cs
+++ equiposWebApi/Controllers/equiposController.cs
@@ -21,20 +21,78 @@ namespace equiposWebApi.Controllers
         }
 
         [HttpGet]
-        [Route("api/equipos/{idUsuario}")]
-        public IActionResult Get(int idUsuario)
+        [Route("api/equipos")]
+        public IActionResult Get()
         {
             IEnumerable<equipos> equiposList = from e in _contexto.equipos
-                                               where e.id_equipos == idUsuario
                                                select e;
-
-            IEnumerable<equipos> equiposListado = _contexto.equipos;
 
             if (equiposList.Count() > 0)
             {
                 return Ok(equiposList);
             }
             return NotFound();
+        }
+
+        [HttpGet]
+        [Route("api/equipos/{idUsuario}")]
+        public IActionResult Get(int idUsuario)
+        {
+            IEnumerable<equipos> equipos = from e in _contexto.equipos
+                                               where e.id_equipos == idUsuario
+                                               select e;
+
+
+            if (equipos.Count() > 0)
+            {
+                return Ok(equipos);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("api/equipos")]
+        public IActionResult guardarEquipo([FromBody] equipos equipoNuevo)
+        {
+            try
+            {
+                _contexto.equipos.Add(equipoNuevo);
+                _contexto.SaveChanges();
+                return Ok(equipoNuevo);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("api/equipos")]
+        public IActionResult updateEquipo([FromBody] equipos equipoModificar)
+        {
+            try
+            {
+                equipos equipoExiste = (from e in _contexto.equipos
+                                        where e.id_equipos == equipoModificar.id_equipos
+                                        select e).FirstOrDefault();
+
+                if (equipoExiste is null)
+                {
+                    return NotFound();
+                }
+
+                equipoExiste.nombre = equipoModificar.nombre;
+                equipoExiste.descripcion = equipoModificar.descripcion;
+
+                _contexto.Entry(equipoExiste).State = EntityState.Modified;
+                _contexto.SaveChanges();
+
+                return Ok(equipoExiste);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
